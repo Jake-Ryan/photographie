7d6b03a8eb94b92f408ce060cb7f9139ddc44cd9
Wakuiy <jake.ryan@accessadvertising.co.uk>
2016-11-04 16:28:14 +0000

    Meh latest stuff.

--- docroot/themes/photographie_theme/js/magnificpopup.js
+++ docroot/themes/photographie_theme/js/magnificpopup.js
@@ -16,6 +16,13 @@
       mainClass: 'mfp-img-mobile mfp-with-zoom',
       image: {
         verticalFit: true
+      },
+      zoom: {
+        enabled: true,
+        duration: 300, // don't foget to change the duration also in CSS
+        opener: function(element) {
+          return element.find('img');
+        }
       }
     });
 
